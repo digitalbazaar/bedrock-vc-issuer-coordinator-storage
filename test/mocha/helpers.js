100041dda6462699a6a73bec3bc637b4ac2b8ceb
Dave Longley <dlongley@digitalbazaar.com>
2025-02-18 00:51:33 -0500

    Store external status storage cursor after status sync.

--- test/mocha/helpers.js
+++ test/mocha/helpers.js
@@ -2,10 +2,12 @@
  * Copyright (c) 2020-2025 Digital Bazaar, Inc. All rights reserved.
  */
 import * as database from '@bedrock/mongodb';
-import {vcReferences} from '@bedrock/vc-issuer-coordinator-storage';
+import {
+  syncRecords, vcReferences
+} from '@bedrock/vc-issuer-coordinator-storage';
 
 export async function cleanDatabase() {
-  // FIXME: add sync state collection
+  await database.collections[syncRecords.COLLECTION_NAME].deleteMany({});
   await database.collections[vcReferences.COLLECTION_NAME].deleteMany({});
 }
 
