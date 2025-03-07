863614c4d8b15bc78faa86c8e93d17fcaabde95c
Dave Longley <dlongley@digitalbazaar.com>
2025-03-07 13:51:30 -0500

    Add `expand` feature and improve credential status matching.

--- test/mocha/20-sync.js
+++ test/mocha/20-sync.js
@@ -4,7 +4,7 @@
 import * as bedrock from '@bedrock/core';
 import * as helpers from './helpers.js';
 import {
-  syncCredentialStatus, utils, vcReferences
+  syncCredentialStatus, vcReferences
 } from '@bedrock/vc-issuer-coordinator-storage';
 import {randomUUID} from 'node:crypto';
 
@@ -88,24 +88,93 @@ describe('Sync API', function() {
     });
 
     it('syncs TerseBitstringStatusEntry credential status', async () => {
-      // test leaving `BitstringStatusListEntry` alone and
-      // expanding `TerseBitstringStatusListEntry`
-      const statusEntries = credentialIds.map((id, index) => {
-        if(index % 0 === 0) {
-          // for even indexes w/cursor below use `BitstringStatusListEntry`
-          return {
-            type: 'BitstringStatusListEntry',
-            statusPurpose: 'revocation'
-          };
-        }
-        // for odd indexes use `TerseBitstringStatusListEntry`
-        return {
-          type: 'TerseBitstringStatusListEntry',
-          terseStatusListBaseUrl:
-            'https://status.example/statuses/1/status-lists',
-          terseStatusListIndex: Math.floor(Math.random() * 4294967295)
-        };
-      });
+      // add a VC w/a terse bitstring status entry (signal in the test suite
+      // is to append `:terse` to the credential ID)
+      {
+        const credentialId = `urn:uuid:${randomUUID()}:terse`;
+        credentialIds.push(credentialId);
+        await vcReferences.insert({
+          reference: {
+            sequence: 0,
+            credentialId
+          }
+        });
+      }
+
+      let err;
+      let result;
+      try {
+        result = await syncCredentialStatus({
+          syncId: 'test1',
+          async getStatusUpdates({cursor = {index: 0}, limit = 100} = {}) {
+            const updates = [];
+            let {index = 0} = cursor;
+            while(index < credentialIds.length) {
+              if(updates.length === limit) {
+                break;
+              }
+              const credentialId = credentialIds[index++];
+              updates.push({
+                credentialId,
+                getCredentialCapability,
+                updateStatusCapability,
+                status: {
+                  // ensure `expand` is only on for credential IDs with
+                  // terse (signal in this test is that the ID ends w/`:terse`)
+                  expand: !credentialId.endsWith(':terse') ? undefined : {
+                    type: 'TerseBitstringStatusListEntry',
+                    // default is also true
+                    required: true,
+                    options: {
+                      statusPurpose: 'revocation'
+                    }
+                  },
+                  credentialStatus: {
+                    type: 'BitstringStatusListEntry',
+                    statusPurpose: 'revocation'
+                  },
+                  value: true
+                }
+              });
+            }
+            return {
+              updates,
+              cursor: {
+                // common field
+                hasMore: index < (credentialIds.length - 1),
+                // use-case specific fields
+                index
+              }
+            };
+          }
+        });
+      } catch(e) {
+        err = e;
+      }
+      assertNoError(err);
+      should.exist(result);
+      result.updateCount.should.equal(4);
+
+      // there should be no update to the reference records
+      for(const credentialId of credentialIds) {
+        const record = await vcReferences.get({credentialId});
+        record.reference.sequence.should.equal(0);
+      }
+    });
+
+    it('syncs w/optional expand feature', async () => {
+      // add a VC w/a terse bitstring status entry (signal in the test suite
+      // is to append `:terse` to the credential ID)
+      {
+        const credentialId = `urn:uuid:${randomUUID()}:terse`;
+        credentialIds.push(credentialId);
+        await vcReferences.insert({
+          reference: {
+            sequence: 0,
+            credentialId
+          }
+        });
+      }
 
       let err;
       let result;
@@ -125,10 +194,18 @@ describe('Sync API', function() {
                 getCredentialCapability,
                 updateStatusCapability,
                 status: {
-                  credentialStatus: utils.expandCredentialStatus({
-                    credentialStatus: statusEntries[index],
+                  expand: {
+                    // do not require expansion
+                    required: false,
+                    type: 'TerseBitstringStatusListEntry',
+                    options: {
+                      statusPurpose: 'revocation'
+                    }
+                  },
+                  credentialStatus: {
+                    type: 'BitstringStatusListEntry',
                     statusPurpose: 'revocation'
-                  }),
+                  },
                   value: true
                 }
               });
@@ -150,7 +227,7 @@ describe('Sync API', function() {
       }
       assertNoError(err);
       should.exist(result);
-      result.updateCount.should.equal(3);
+      result.updateCount.should.equal(4);
 
       // there should be no update to the reference records
       for(const credentialId of credentialIds) {
